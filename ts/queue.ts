3797f27c4ba9cd2c679d81824a5b0f66835909a4
Trevor Sears <trevors@knorrmarketing.com>
2021-09-13 12:42:00 -0400

    Completely reworked queue with doubly linked list implementation.

--- ts/queue.ts
+++ ts/queue.ts
@@ -20,87 +20,135 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import { DoublyLinkedList } from "@jsdsl/doubly-linked-list";
+
 /**
- * A queue/FIFO data structure.
- *
- * @author Trevor Sears <trevorsears.main@gmail.com>
- * @version v0.1.0
+ * A queue (FIFO) implementation written in TypeScript.
+ * 
+ * @author Trevor Sears <trevor@trevorsears.com> (https://trevorsears.com/)
+ * @version v1.0.0
  * @since v0.1.0
  */
 export class Queue<E = any> {
 	
-	private internalArray: E[] = [];
+	/**
+	 * The internal doubly linked list data structure that is used to maintain this queue.
+	 */
+	private internalDLL: DoublyLinkedList<E>;
 	
+	/**
+	 * Initializes a new Queue with the provided elements.
+	 * 
+	 * @param {E} elements An optional list of elements to enqueue into this queue.
+	 */
 	public constructor(...elements: E[]) {
+		
+		this.internalDLL = new DoublyLinkedList<E>();
 		
 		this.enqueue(...elements);
 		
 	}
 	
+	/**
+	 * Inserts an element at the start of this queue.
+	 * 
+	 * @param {E} elements The element to insert into this queue.
+	 */
 	public enqueue(...elements: E[]): void {
 		
-		this.internalArray.push(...elements);
+		for (let element of elements) this.internalDLL.insertFirst(element);
 		
 	}
 	
+	/**
+	 * Returns and removes the element at the end of this queue if such an element exists, otherwise returning
+	 * undefined.
+	 * 
+	 * @returns {E | undefined} The element at the end of this queue if such an element exists, otherwise undefined.
+	 */
 	public dequeue(): E | undefined {
 		
-		return this.internalArray.shift();
+		return this.internalDLL.removeLast();
 		
 	}
 	
+	/**
+	 * Returns the element at the end of this queue (without removing it) if such an element exists, otherwise returning
+	 * undefined.
+	 * 
+	 * @returns {E | undefined} The element at the end of this queue if such an element exists, otherwise undefined.
+	 */
 	public peek(): E | undefined {
 		
-		return this.internalArray[0];
+		return this.internalDLL.getLast();
 		
 	}
 	
+	/**
+	 * Randomly reorders the elements of this queue over the number of specified iterations, defaulting to 1 iteration.
+	 * 
+	 * @param {number} iterations The number of iterations over which to shuffle the elements of this queue.
+	 */
 	public shuffle(iterations: number = 1): void {
 		
-		for (let count: number = 0; count < iterations; count++) {
-			
-			let elements: E[] = this.toArray();
-			this.clear();
-			
-			while (elements.length !== 0) {
-				
-				let random: number = Math.floor(Math.random() * elements.length);
-				let element: E = elements.splice(random, 1)[0];
-				this.enqueue(element);
-				
-			}
-			
-		}
+		this.internalDLL.shuffle(iterations);
 		
 	}
 	
+	/**
+	 * Returns an integer representative of the number of elements present in this queue.
+	 * 
+	 * @returns {number} An integer representative of the number of elements present in this queue.
+	 */
 	public size(): number {
 		
-		return this.internalArray.length;
+		return this.internalDLL.size();
 		
 	}
 	
+	/**
+	 * Returns true if this queue contains no elements.
+	 * 
+	 * @returns {boolean} true if this queue contains no elements.
+	 */
 	public isEmpty(): boolean {
 		
-		return (this.size() === 0);
+		return this.internalDLL.isEmpty();
 		
 	}
 	
+	/**
+	 * Returns true if this queue contains the specified element.
+	 * 
+	 * @param {E} element The element to search for in this queue.
+	 * @returns {boolean} true if this queue contains the specified element.
+	 */
 	public contains(element: E): boolean {
 		
-		return this.internalArray.includes(element);
+		return this.internalDLL.contains(element);
 		
 	}
 	
+	/**
+	 * Removes all elements from this queue, rendering it empty.
+	 */
 	public clear(): void {
 		
-		this.internalArray = [];
+		this.internalDLL.clear();
 		
 	}
 	
+	/**
+	 * Converts this queue to an array, returning the result.
+	 * 
+	 * The resulting array will be ordered such that those elements at the end of the queue will also appear near the
+	 * end of the resultant array.
+	 * 
+	 * @returns {E[]} An array representation of this queue. 
+	 */
 	public toArray(): E[] {
 		
-		return this.internalArray;
+		return this.internalDLL.toArray();
 		
 	}
 	
